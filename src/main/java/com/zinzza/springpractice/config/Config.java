e4383aab46fac19e6ac98f003998eb3d6c95f4c8
Han-Bin Song <thdgksq@naver.com>
2023-02-05 16:46:14 +0900

    Done #1 - spring web MVC
    스프링 web mvc 적용 완성

--- src/main/java/com/zinzza/springpractice/config/Config.java
+++ src/main/java/com/zinzza/springpractice/config/Config.java
@@ -2,7 +2,9 @@ package com.zinzza.springpractice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.zinzza.springpractice")
 @Configuration
 public class Config {
