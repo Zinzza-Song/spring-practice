6348152ce2b70faaa6c7aa3715862d9b26cd8edc
Han-Bin Song <thdgksq@naver.com>
2023-02-05 16:06:12 +0900

    Config #1 - spring configuration
    기초 설정
    루트 패키지 이하에 빈이 있는지 스캔한다.

---
+++ src/main/java/com/zinzza/springpractice/config/Config.java
@@ -0,0 +1,9 @@
+package com.zinzza.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.zinzza.springpractice")
+@Configuration
+public class Config {
+}
